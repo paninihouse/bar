6d165c74a5a6b471c155b12ab15ef833d6b2aa36
Tommaso Negri <me@tommasongr.com>
2026-07-26 16:31:35 +0200

    add main file

---
+++ Sources/phbar/main.swift
@@ -0,0 +1,8 @@
+import AppKit
+
+// Entry point. Top-level code runs on the main actor.
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.setActivationPolicy(.accessory)   // no Dock icon, no menu
+app.run()
