3759cbb8e11698c3fa76c2e90c77bbdada5cba18
Tommaso Negri <me@tommasongr.com>
2026-08-08 13:07:42 +0200

    cleanup comments

--- Sources/phbar/main.swift
+++ Sources/phbar/main.swift
@@ -4,5 +4,5 @@ import AppKit
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
-app.setActivationPolicy(.accessory)   // no Dock icon, no menu
+app.setActivationPolicy(.accessory) // no Dock icon, no menu
 app.run()
